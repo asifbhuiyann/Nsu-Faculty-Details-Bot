c0336a248bff63ac762407b567b7ed916416bab5
asifbhuiyann <getconnectwithasif@gmail.com>
2023-12-21 19:27:11 +0600

    Added some changes in the structure

---
+++ nsubot.php
@@ -0,0 +1,17 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "nsu-faculty-details") or die("Database Error");
+$getMesg = mysqli_real_escape_string($conn, $_POST['text']);
+
+$check_data = "SELECT response FROM nsu_bot WHERE queries LIKE '%$getMesg%'";
+$run_query = mysqli_query($conn, $check_data) or die("Error");
+
+if(mysqli_num_rows($run_query) > 0){
+    //fetching replay from the database according to the user query
+    $fetch_data = mysqli_fetch_assoc($run_query);
+    //storing replay to a varible which we'll send to ajax
+    $replay = $fetch_data['response'];
+    echo $replay;
+}else{
+    echo "Sorry can't be able to understand you!";
+}
+?>
